aa9e5eedb81e198f57a0447a7de5ab9127eab3b0
taeyoung <limbirdy@naver.com>
2025-01-09 11:05:12 +0900

    springbootTest

---
+++ src/main/java/com/jbedu/test/SpringbootTestProject250107Application.java
@@ -0,0 +1,13 @@
+package com.jbedu.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestProject250107Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestProject250107Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jbedu/test/SpringbootTestProject250107ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbedu.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestProject250107ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
